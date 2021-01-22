7b5a7db544547ad79baf13a41913f78873ec579c
Hasan Diwan <hd1@jsc.d8u.us>
2021-01-22 07:53:10 -0800

    add frequency for autorenew

--- db/migrate/20210122074456_add_autorenew_to_tasks.rb
+++ db/migrate/20210122074456_add_autorenew_to_tasks.rb
@@ -1,6 +1,5 @@
 class AddAutorenewToTasks < ActiveRecord::Migration[5.1]
   def change
-    add_column :tasks, :frequency, :integer, default: -1
-    add_column :tasks, :is_ephemeral, :boolean, default: false
+    add_column :tasks, :frequency, :integer, default: 0
   end
 end
